145c6d0c850591843ccfbe7ea33fabf251fe60f8
ZachParris <zacharyjparris@gmail.com>
2016-02-06 13:09:34 -0600

    created plan for JS

---
+++ main.js
@@ -0,0 +1,30 @@
+//create product array
+
+//create product objects
+
+//name description price URL
+
+
+
+
+
+
+//ittirate thru array
+
+
+
+
+
+//append to the DOM
+
+
+
+
+//add CSS thru JS
+
+
+
+
+
+//repeat for chips
+
